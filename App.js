bfbad6742fe71d44b9e4e6d28bc4678ce15ad319
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-06-14 15:25:49 -0700

    Add Progress Bar component

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import ProgressBar from './ProgressBar';
 
 export default class App extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      startButtonText: 'Start',
+      timerLimit: 0,
+      timerRunning: false
+    }
+  }
+
+  startTimer = () => {
+    this.setState({ timerRunning: true, startButtonText: 'Pause', timerLimit: 10 });
+  }
+
+  stopTimer = () => {
+    this.setState({ timerRunning: false, startButtonText: 'Start', timerLimit: 0 });
+  }
+
+  pauseTimer = () => {
+    this.setState({ timerRunning: false, startButtonText: 'Resume' });
+  }
+
+  timerFinish = () => {
+    console.log("FINISH")
+    this.setState({ timerRunning: false, startButtonText: 'Start', timerLimit: 0  });
+  }
+
   render() {
+    displayProgressBar = () => {
+      if(this.state.timerRunning){
+        return <ProgressBar maxTime={this.state.timerLimit} timerFinish={this.timerFinish}/>
+      } else {
+        return null;
+      }
+    }
+
     return (
       <View style={styles.container}>
-        <Text>High interval</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.welcome}>
+          Welcome to High Intensity Interval Training!
+        </Text>
+        <Button
+          onPress={!this.state.timerRunning ? this.startTimer : this.pauseTimer}
+          title={this.state.startButtonText}
+          color="#0909ea"
+        />
+        <Button
+          onPress={this.stopTimer}
+          title="Stop"
+          color="#841584"
+        />
+        {displayProgressBar()}
       </View>
     );
   }
@@ -15,7 +62,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5FCFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
